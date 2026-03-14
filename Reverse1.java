07f2e53b86507cfd1627fd0f1e74e2fbd49ab06e
KeerthiReddy520 <keerthireddybandi.kr@gmail.com>
2026-03-14 10:06:19 +0530

    Add files via upload

---
+++ Reverse1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Reverse1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a String:");
+        String str=sc.nextLine();
+        String reversed=" ";
+        for (int i=str.length()-1;i>=0;i--)
+        {
+            reversed=reversed+str.charAt(i);
+        }
+        System.out.println("reversed="+ reversed);
+    }
+}
